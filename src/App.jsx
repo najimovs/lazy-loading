b1a285561dd9d8d3e4a761e04f62f4f71d3cfc89
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-17 15:24:43 +0500

    Add some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { Routes, Route } from "react-router"
+import Home from "./Components/Home"
+import About from "./Components/About"
+import Contacts from "./Components/Contacts"
+
 export default function () {
 
 	return (
-		<>App</>
+		<Routes>
+			<Route path="/" element={ <Home /> } />
+			<Route path="/about" element={ <About /> } />
+			<Route path="/contacts" element={ <Contacts /> } />
+		</Routes>
 	)
 }
